aa7d720f23ecc7eefd031b5d9054e04e09c82c9d
GabrielHertal <137331212+GabrielHertal@users.noreply.github.com>
2024-11-27 20:23:06 -0300

    faltou esse

--- db/db_epis.sql
+++ db/db_epis.sql
@@ -1,149 +1,150 @@
-CREATE DATABASE  IF NOT EXISTS `db_epis` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
-USE `db_epis`;
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: db_epis
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+  CREATE DATABASE  IF NOT EXISTS `db_epis` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+  USE `db_epis`;
+  -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+  --
+  -- Host: 127.0.0.1    Database: db_epis
+  -- ------------------------------------------------------
+  -- Server version	5.5.5-10.4.32-MariaDB
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
---
--- Table structure for table `colaboradores`
---
-DROP TABLE IF EXISTS `colaboradores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `colaboradores` (
-  `id_colaborador` int(11) NOT NULL AUTO_INCREMENT,
-  `nome_colaborador` varchar(100) NOT NULL,
-  `data_nascimento` date DEFAULT NULL,
-  `cpf_cnpj` varchar(100) NOT NULL,
-  `rg` varchar(100) NOT NULL,
-  `ativo` tinyint(1) DEFAULT NULL,
-  `telefone` varchar(100) DEFAULT NULL,
-  `imagem_colaborador` varchar(255) DEFAULT NULL,
-  `id_departamento` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_colaborador`),
-  KEY `fk_id_departamento_idx` (`id_departamento`),
-  CONSTRAINT `fk_id_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `departamentos`
---
-DROP TABLE IF EXISTS `departamentos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `departamentos` (
-  `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
-  `nome_departamento` varchar(255) DEFAULT NULL,
-  `ativo` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id_departamento`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `emprestimos`
---
-DROP TABLE IF EXISTS `emprestimos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `emprestimos` (
-  `id_emprestimo` int(11) NOT NULL AUTO_INCREMENT,
-  `data_emprestimo` datetime DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL,
-  `data_devolucao` datetime DEFAULT NULL,
-  `observacoes` varchar(100) DEFAULT NULL,
-  `colaborador` int(11) DEFAULT NULL,
-  `situacao` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id_emprestimo`),
-  KEY `emprestimos_colaboradores_FK` (`colaborador`),
-  KEY `emprestimos_usuarios_FK` (`usuario`),
-  CONSTRAINT `emprestimos_colaboradores_FK` FOREIGN KEY (`colaborador`) REFERENCES `colaboradores` (`id_colaborador`),
-  CONSTRAINT `emprestimos_usuarios_FK` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `endereco_colaborador`
---
-DROP TABLE IF EXISTS `endereco_colaborador`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `endereco_colaborador` (
-  `fk_id_colaborador` int(11) DEFAULT NULL,
-  `rua` varchar(255) DEFAULT NULL,
-  `numero` varchar(15) DEFAULT NULL,
-  `bairro` varchar(100) DEFAULT NULL,
-  `cidade` varchar(255) DEFAULT NULL,
-  `uf` char(2) DEFAULT NULL,
-  `cep` varchar(10) DEFAULT NULL,
-  `complemento` varchar(255) DEFAULT NULL,
-  KEY `fk_id_colaborador_idx` (`fk_id_colaborador`),
-  CONSTRAINT `fk_id_colaborador` FOREIGN KEY (`fk_id_colaborador`) REFERENCES `colaboradores` (`id_colaborador`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `equipamentos`
---
-DROP TABLE IF EXISTS `equipamentos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `equipamentos` (
-  `id_equipamento` int(11) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(100) DEFAULT NULL,
-  `ativo` tinyint(1) DEFAULT NULL,
-  `qtd_estoque` int(11) DEFAULT NULL,
-  `certificado_aprovacao` varchar(100) DEFAULT NULL,
-  `imagem_equipamento` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_equipamento`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `equipamentos_emprestimo`
---
-DROP TABLE IF EXISTS `equipamentos_emprestimo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `equipamentos_emprestimo` (
-  `id_equipamento_emprestimo` int(11) NOT NULL AUTO_INCREMENT,
-  `equipamento` int(11) DEFAULT NULL,
-  `emprestimo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_equipamento_emprestimo`),
-  KEY `equipamentos_emprestimo_equipamentos_FK` (`equipamento`),
-  KEY `FK_EMPRESTIMO_idx` (`emprestimo`),
-  CONSTRAINT `FK_EMPRESTIMO` FOREIGN KEY (`emprestimo`) REFERENCES `emprestimos` (`id_emprestimo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_EQUIPAMENTO` FOREIGN KEY (`equipamento`) REFERENCES `equipamentos` (`id_equipamento`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Table structure for table `usuarios`
---
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `nome_usuario` varchar(100) DEFAULT NULL,
-  `senha` varchar(100) DEFAULT NULL,
-  `ativo` tinyint(1) DEFAULT NULL,
-  `administrador` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+  /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+  /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+  /*!50503 SET NAMES utf8 */;
+  /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+  /*!40103 SET TIME_ZONE='+00:00' */;
+  /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+  /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+  /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+  /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+  --
+  -- Table structure for table `colaboradores`
+  --
+  DROP TABLE IF EXISTS `colaboradores`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `colaboradores` (
+    `id_colaborador` int(11) NOT NULL AUTO_INCREMENT,
+    `nome_colaborador` varchar(100) NOT NULL,
+    `data_nascimento` date DEFAULT NULL,
+    `cpf_cnpj` varchar(100) NOT NULL,
+    `rg` varchar(100) NOT NULL,
+    `ativo` tinyint(1) DEFAULT NULL,
+    `telefone` varchar(100) DEFAULT NULL,
+    `imagem_colaborador` varchar(255) DEFAULT NULL,
+    `id_departamento` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id_colaborador`),
+    KEY `fk_id_departamento_idx` (`id_departamento`),
+    CONSTRAINT `fk_id_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `departamentos`
+  --
+  DROP TABLE IF EXISTS `departamentos`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `departamentos` (
+    `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
+    `nome_departamento` varchar(255) DEFAULT NULL,
+    `ativo` tinyint(1) DEFAULT NULL,
+    PRIMARY KEY (`id_departamento`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `emprestimos`
+  --
+  DROP TABLE IF EXISTS `emprestimos`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `emprestimos` (
+    `id_emprestimo` int(11) NOT NULL AUTO_INCREMENT,
+    `data_emprestimo` datetime DEFAULT NULL,
+    `usuario` int(11) DEFAULT NULL,
+    `data_devolucao` datetime DEFAULT NULL,
+    `observacoes` varchar(100) DEFAULT NULL,
+    `colaborador` int(11) DEFAULT NULL,
+    `situacao` tinyint(1) DEFAULT NULL,
+    PRIMARY KEY (`id_emprestimo`),
+    KEY `emprestimos_colaboradores_FK` (`colaborador`),
+    KEY `emprestimos_usuarios_FK` (`usuario`),
+    CONSTRAINT `emprestimos_colaboradores_FK` FOREIGN KEY (`colaborador`) REFERENCES `colaboradores` (`id_colaborador`),
+    CONSTRAINT `emprestimos_usuarios_FK` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id_usuario`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `endereco_colaborador`
+  --
+  DROP TABLE IF EXISTS `endereco_colaborador`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `endereco_colaborador` (
+    `fk_id_colaborador` int(11) DEFAULT NULL,
+    `rua` varchar(255) DEFAULT NULL,
+    `numero` varchar(15) DEFAULT NULL,
+    `bairro` varchar(100) DEFAULT NULL,
+    `cidade` varchar(255) DEFAULT NULL,
+    `uf` char(2) DEFAULT NULL,
+    `cep` varchar(10) DEFAULT NULL,
+    `complemento` varchar(255) DEFAULT NULL,
+    KEY `fk_id_colaborador_idx` (`fk_id_colaborador`),
+    CONSTRAINT `fk_id_colaborador` FOREIGN KEY (`fk_id_colaborador`) REFERENCES `colaboradores` (`id_colaborador`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `equipamentos`
+  --
+  DROP TABLE IF EXISTS `equipamentos`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `equipamentos` (
+    `id_equipamento` int(11) NOT NULL AUTO_INCREMENT,
+    `descricao` varchar(100) DEFAULT NULL,
+    `ativo` tinyint(1) DEFAULT NULL,
+    `qtd_estoque` int(11) DEFAULT NULL,
+    `certificado_aprovacao` varchar(100) DEFAULT NULL,
+    `imagem_equipamento` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id_equipamento`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `equipamentos_emprestimo`
+  --
+  DROP TABLE IF EXISTS `equipamentos_emprestimo`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `equipamentos_emprestimo` (
+    `id_equipamento_emprestimo` int(11) NOT NULL AUTO_INCREMENT,
+    `equipamento` int(11) DEFAULT NULL,
+    `emprestimo` int(11) DEFAULT NULL,
+    `quantidade` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id_equipamento_emprestimo`),
+    KEY `equipamentos_emprestimo_equipamentos_FK` (`equipamento`),
+    KEY `FK_EMPRESTIMO_idx` (`emprestimo`),
+    CONSTRAINT `FK_EMPRESTIMO` FOREIGN KEY (`emprestimo`) REFERENCES `emprestimos` (`id_emprestimo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `FK_EQUIPAMENTO` FOREIGN KEY (`equipamento`) REFERENCES `equipamentos` (`id_equipamento`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  --
+  -- Table structure for table `usuarios`
+  --
+  DROP TABLE IF EXISTS `usuarios`;
+  /*!40101 SET @saved_cs_client     = @@character_set_client */;
+  /*!50503 SET character_set_client = utf8mb4 */;
+  CREATE TABLE `usuarios` (
+    `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+    `nome_usuario` varchar(100) DEFAULT NULL,
+    `senha` varchar(100) DEFAULT NULL,
+    `ativo` tinyint(1) DEFAULT NULL,
+    `administrador` tinyint(1) DEFAULT NULL,
+    PRIMARY KEY (`id_usuario`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  /*!40101 SET character_set_client = @saved_cs_client */;
+  /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+  /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+  /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+  /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+  /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+  /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
